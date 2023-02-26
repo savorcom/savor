16dde088f9891daf88f31e9ab09b7db1dd911790
Mike Riley <mikeriley131@gmail.com>
2023-02-26 16:33:14 -0700

    adds header background transition

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import AnchorLink from 'react-anchor-link-smooth-scroll-v2';
 
 import savorLogo from '../images/savor-logo.svg';
 
-const Header = ({ menuItems }) => (
-  <header className="header" role="banner">
-    <div className="container">
-      <img className="header__logo" src={savorLogo} alt="Savor logo" />
+const Header = ({ menuItems }) => {
+  useEffect(() => {
+    let scrollPos = window.scrollY;
+    const header = document.querySelector('header');
+    // const headerHeight = header.offsetHeight;
 
-      <nav className="nav">
-        <ul className="nav__list">
-          {menuItems.map((item) => (
-            <li className="nav__item" key={item.label}>
-              <AnchorLink className="nav__link" href={item.path} offset="24">
-                {item.label}
-              </AnchorLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
-  </header>
-);
+    const addClassOnScroll = () => header.classList.add('header__background');
+    const removeClassOnScroll = () =>
+      header.classList.remove('header__background');
+
+    window.addEventListener('scroll', () => {
+      scrollPos = window.scrollY;
+
+      if (scrollPos >= 1) {
+        addClassOnScroll();
+      } else {
+        removeClassOnScroll();
+      }
+
+      console.log(scrollPos);
+    });
+  }, []);
+
+  return (
+    <header className="header" role="banner">
+      <div className="container">
+        <AnchorLink className="header__logo-link" href="/">
+          <img className="header__logo" src={savorLogo} alt="Savor logo" />
+        </AnchorLink>
+
+        <nav className="nav">
+          <ul className="nav__list">
+            {menuItems.map((item) => (
+              <li className="nav__item" key={item.label}>
+                <AnchorLink className="nav__link" href={item.path} offset="108">
+                  {item.label}
+                </AnchorLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   menuItems: PropTypes.arrayOf(
