d911d4b693e312617ba2a9c3f25f9a14237ff4a5
Mike Riley <mikeriley131@gmail.com>
2023-04-11 20:02:44 -0600

    removes drip animation

--- src/components/drip.jsx
+++ src/components/drip.jsx
@@ -1,33 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import drip from '../images/header-drip.png';
 
-const Drip = () => {
-  useEffect(() => {
-    let scrollPos = window.scrollY;
-    const header = document.getElementById('drip');
-
-    const addSlideUpOnScroll = () => header.classList.add('slideUp');
-    const addSlideDownOnScroll = () => header.classList.add('slideDown');
-    const removeSlideUpOnScroll = () => header.classList.remove('slideUp');
-    const removeSlideDownOnScroll = () => header.classList.remove('slideDown');
-    const removeDripInitial = () => header.classList.remove('drip--initial');
-
-    window.addEventListener('scroll', () => {
-      scrollPos = window.scrollY;
-
-      if (scrollPos >= 1) {
-        removeDripInitial();
-        removeSlideDownOnScroll();
-        addSlideUpOnScroll();
-      } else {
-        removeSlideUpOnScroll();
-        addSlideDownOnScroll();
-      }
-    });
-  }, []);
-
-  return <img id="drip" className="drip drip--initial" src={drip} alt="" />;
-};
+const Drip = () => <img id="drip" className="drip" src={drip} alt="" />;
 
 export default Drip;
