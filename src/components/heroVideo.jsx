357a611d25502b560c23f0396ca5103346af39f1
Mike Riley <mikeriley131@gmail.com>
2023-04-14 20:37:42 -0600

    use local video file until production ready

--- src/components/heroVideo.jsx
+++ src/components/heroVideo.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import heroVideoFile from '../images/butter.mp4';
+
 const heroVideo = ({ videoUrl }) => (
   <div className="hero__video">
     <div className="iframe-container iframe-container-16x9">
       <video className="hero__video-video" muted autoPlay loop>
-        <source src={videoUrl} type="video/mp4" />
+        <source src={heroVideoFile} type="video/mp4" />
       </video>
     </div>
   </div>
