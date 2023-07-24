f87eeffcce30c7bc7f337788a9303559d8c1681a
Mike Riley <mikeriley131@gmail.com>
2023-07-24 13:45:59 -0600

    replaces video poster

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -2,9 +2,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import poster from '../images/video-thumbnail.jpg';
+
 const Video = ({ videoFileMp4, videoFileWebm }) => (
   <div className="iframe-container iframe-container-16x9">
-    <video controls>
+    <video controls poster={poster}>
       <source src={videoFileWebm} type="video/webm" />
       <source src={videoFileMp4} type="video/mp4" />
     </video>
